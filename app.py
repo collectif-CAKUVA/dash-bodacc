bde544c2b401971076bdd3ec83ce87ecef0bef94
clement <clementcamara@gmail.com>
2021-03-05 12:39:52 +0100

    feature-dropdown-graph-one

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import dash
 import dash_core_components as dcc
 import plotly.express as px
 import dash_html_components as html
-
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 
 """ 
 graph 1 --> date + activite + code APE 
@@ -13,15 +14,30 @@ Data:
 --> activite de l'insee
 """
 
+# ['code_ape', 'activte_insee', 'date_publication', 'size']
+
 """ création de dataframe et du graph --> Dash / plotly"""
 app = dash.Dash(__name__)
 # print(df)
-fig = px.line(df2, x="date_publication", y="size", color="code_ape", template='plotly_dark')
+fig = px.line(df_final, x="date_publication", y="size", color="code_ape",
+              line_group='activte_insee', template='plotly_dark')
 
 """ html display """
 app.layout = html.Div(children=[
-    dcc.Graph(className='row', id='example-graph', figure=fig),
+    dcc.Dropdown(id='selector', options=[{'label': 'CODE APE', 'value': 'code_ape'},
+                                         {'label': 'INSEE', 'value': 'activte_insee'}],
+                 className='selector'),
+    dcc.Graph(className='row', id='bodacc-graph', figure=fig)
 ])
+
+
+@app.callback(
+    Output('bodacc-graph', 'figure'),
+    Input('selector', 'value'))
+def update_graph(value):
+    figure = px.line(df_final, x="date_publication", y="size", color=value,
+                     line_group='activte_insee', template='plotly_dark')
+    return figure
 
 """ run app"""
 if __name__ == '__main__':
--- dataframe.py
+++ dataframe.py
@@ -9,10 +9,26 @@ Index(['Unnamed: 0', 'siren', 'departement', 'date_publication',
 
 """ transforme CSV to dataframe  """
 df = pd.read_csv("bodacc_janvier_fevrier.csv")
-#print(df.columns)
 
-df2 = df.groupby(['code_ape', 'activte_insee', 'date_publication'], as_index=False).size()
-df2['size']
-df3 = df2.sort_values(by='size', ascending=False)
-df4 = df3.head(5)
-print(df4)
+df_p = df.groupby(['code_ape']).size().sort_values(ascending=False).head(12)
+
+
+#df2 = df.groupby(['code_ape', 'activte_insee', 'date_publication'], as_index=False).size()
+
+
+df_top = df.loc[df.code_ape.isin(df_p.index), ['code_ape', 'date_publication', 'activte_insee']]
+print(df_top.code_ape.unique())
+df_final = df_top.groupby(['code_ape', 'activte_insee', 'date_publication'], as_index=False).size()
+print(df_final)
+
+
+
+
+# df_top = df2[(df2['code_ape'] == '68.20B') & (df2['code_ape'] == '53.20Z') & (df2['code_ape'] == '70.10Z')
+# & (df2['code_ape'] == '56.10C')]
+
+
+# print(df_top)
+# df_top = df2[(df2['size'] > 100)]
+
+
