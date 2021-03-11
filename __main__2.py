22bbcdb6cd8c86cbd2e3783489147541d3c6c22c
Pierre Kuhnmunch <pierrekuh@yahoo.fr>
2021-03-11 16:14:17 +0100

    'DB2'

--- __main__2.py
+++ __main__2.py
@@ -3,6 +3,7 @@ import time
 import glob, os
 from download_extract import download, extract_tar, test
 from parserr import parsing
+import database
 
 """dowloading + extracting with download_extract.py"""
 download()
@@ -19,6 +20,8 @@ start_time = time.time()
 for file in glob.glob("*.xml"):
     nb_fil_total += 1
 print(nb_fil_total)
+
+
 
 for file in glob.glob("*.xml"):
 
@@ -39,10 +42,7 @@ for file in glob.glob("*.xml"):
         total_entrees += 1
         print(f'Entrée n°{nb_entree_ds_fichier} de {file} pour {total_entrees} entrées totales')
         liste = parsing(root1,date)
-
-
-print(len(liste))
-
+        database.add_entreprise(liste)
 """ counter """
 end_time = time.time()
 temps = end_time - start_time
---
+++ database.py
@@ -0,0 +1,103 @@
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column, Text, DateTime, ForeignKey, Numeric, Float, \
+    Date, and_
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import relationship
+import random
+from datetime import datetime
+# pour postgres
+
+
+logf = open("download.log", "w")
+
+engine = create_engine(
+    'postgresql+psycopg2://Cakuva:Collectif33PierreThibaultClementSimplon@cakuva-bodacc.c9yrikhahmsh.eu-west-3.rds.amazonaws.com/postgres')
+
+# Pour sqlite3:
+# engine = create_engine('sqlite:///sqlite3.db')  # using relative path and sqlit3
+
+# When using SQLAlchemy ORM, we interact with the database using the Session object.
+# The Session object also wraps the database connection and transaction.
+# The transaction implicitly starts as soon as the Session starts communicating with the database
+# and will remain open until the Session is committed, rolled back or closed.
+
+Session = sessionmaker(bind=engine)
+
+# then connect the engine
+
+engine.connect()
+
+Base = declarative_base()
+
+
+# Defining Models
+# A Model is a Python class which corresponds to the
+# database table and its attributes represent the columns.
+# see Defining Relationships @ https://overiq.com/sqlalchemy-101/defining-schema-in-sqlalchemy-orm/
+
+
+
+
+class Entreprise(Base):
+    __tablename__ = 'entreprises'
+    siren = Column(String(100), primary_key=True)
+    juridic_form = Column(String(300),nullable=False)
+    company_activity = Column(Text, nullable=False)
+    insee_activity = Column(Text, nullable=False)
+    ape = Column(String(100), nullable=False)
+    code_postal = Column(String(100))
+    imatriculation_date = Column(Date)
+
+
+
+# On exécute cette methode, pour créer les tables dans la DB
+def create_tables():
+    Base.metadata.create_all(engine)
+
+
+# Pour supprimer toute les tables
+def drop_tables():
+    Base.metadata.drop_all(engine)
+
+# Pour ajouter des données sur la table "entreprise"
+# Problème restant ici, notamment au niveau de la duplication de clé primaire ( plusieurs fois le même SIREN dans le CSV )
+# Il faut egalement gérer le principe de clé primaire / secondaire car non fonctionnel pour le moment
+
+def add_entreprise(data):
+
+
+    session = Session()
+    compteur = 0
+    for entreprise in data:
+
+        try:
+
+            company = Entreprise(siren=entreprise['siren'],
+            juridic_form=entreprise['forme_juridique'],
+            company_activity=entreprise['activite'],
+            insee_activity=entreprise['activite_insee'],
+            ape=entreprise['code_ape'],
+            code_postal=entreprise['code_postal'],
+            imatriculation_date=entreprise['date_immat'])
+
+
+            print(f'line {compteur} added to session')
+            compteur += 1
+
+
+            session.add(company)
+            session.commit()
+        # code to process download here
+        except Exception as e:  # most generic exception you can catch
+            logf.write("Failed to insert in DB {0}: {1}\n".format(str(entreprise), str(e)))
+        # optional: delete local version of failed download
+
+    print('the session has been commited, and datas saved in DB ')
+
+drop_tables()
+create_tables()
+ # CRUD
+
+# Create
